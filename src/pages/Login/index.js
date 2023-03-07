7fdd562c84d9bd61c96adc26453bb557fd9dbcdf
birong <linbirong22@163.com>
2023-03-07 14:02:38 +0100

    项目初始化 antd组件库的使用

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import { Button } from "antd";
 
 export default class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login
+        <Button type="primary">我是一个按钮</Button>
+      </div>
+    );
   }
 }
